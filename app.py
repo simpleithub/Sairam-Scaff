9604108fa68e8d7ed030ca4a2802c0d56f5bcfbe
Sai Geetha <md.saigeetha@gmail.com>
2025-05-26 19:51:39 +0530

    Updated images and code

--- app.py
+++ app.py
@@ -6,11 +6,15 @@ app = Flask(__name__, static_folder='.', static_url_path='')
 with open('templates/index.html', 'r', encoding='utf-8') as f:
     html_content = f.read()
 
+# Read the Sairam Tech HTML as a template string
 with open('templates/sairam_tech.html', 'r', encoding='utf-8') as f:
     sairam_tech_content = f.read()
 
 with open('templates/saira-enterprises.html', 'r', encoding='utf-8') as f:
     saira_enterprises_content = f.read()
+
+with open('templates/custom-solutions.html', 'r', encoding='utf-8') as f:
+    custom_solutions_content = f.read()
 
 @app.route('/')
 def home():
@@ -28,6 +32,13 @@ def styles():
 def sairam_tech_css():
     return send_from_directory('static', 'sairam_tech.css')
 
-@app.route('/saira_enterprises')
+@app.route('/sairam_enterprises')
 def saira_enterprises():
     return render_template_string(saira_enterprises_content)
+
+@app.route('/custom_solutions')
+def custom_solutions():
+    return render_template_string(custom_solutions_content)
+
+if __name__ == '__main__':
+    app.run(debug=True)
